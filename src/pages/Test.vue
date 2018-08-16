ff5cbcf9f275a4d4364a5d8004b5d242562057e7
paiizz <wanchanapon.t@ku.th>
2018-08-16 17:34:02 +0700

    learn vue

---
+++ src/pages/Test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    test {{id}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      id: ''
+    }
+  },
+  created () {
+    const id = this.$route.params.id
+    this.id = id
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/Todo.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div>
+      <HelloWorld :message="fullName" @add="addToDo">
+        <template slot="greeting">Hello</template>
+      </HelloWorld>
+    </div>
+    <div>
+      Firstname : <input type="text" v-model="firstName">
+    </div>
+    <div>
+      Lastname : <input type="text" v-model="lastName">
+    </div>
+    <div>
+      Fullname : {{fullName}}
+    </div>
+    <List />
+    <button @click="test">Go to test</button>
+  </div>
+</template>
+
+<script>
+
+import HelloWorld from '@/components/HelloWorld'
+import List from '@/components/List'
+import { mapActions } from 'vuex'
+export default {
+  name: 'Todo',
+  data () {
+    return {
+      firstName: '',
+      lastName: ''
+    }
+  },
+  computed: {
+    fullName () {
+      return `${this.firstName} ${this.lastName}`
+    }
+  },
+  watch: {
+    firstName (newValue, oldValue) {
+      // console.log('newValue = ' + newValue)
+      // console.log('oldValue = ' + oldValue)
+    }
+  },
+  methods: {
+    ...mapActions({
+      setList: 'setList'
+    }),
+    addToDo (name) {
+      this.setList(name)
+    },
+    test () {
+      this.$router.push({name: 'Test', params: { id: 100 }})
+    }
+  },
+  components: {
+    HelloWorld,
+    List
+  },
+  created () {
+    console.log('created working ...')
+  },
+  mounted () {
+    console.log('mounted working ...')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
